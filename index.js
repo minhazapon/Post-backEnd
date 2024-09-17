ad6b8bfc13f73b99772de5d1beee4cef7ae186a3
minhazapon <minhajulabedinapon@gmail.com>
2024-09-17 08:07:05 -0700

    added file

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const app = express()
 const port = process.env.port || 5000
 
 app.use(cors())
+app.use(express.json());
 
   
- const user = [
-     
+const user = [
      { id:1, name:'minhazul abedin', position:'full stack Developer', language:'javasCript', company:'toptal', email:'apon@gmail.com'},
      { id:2, name:'Hm nayeem', position:'full stack Developer', language:'javasCript', company:'toptal', email:'nayeem@gmail.com'},
      { id:3, name:'Jhankar mahbub', position:'full stack Developer', language:'javasCript', company:'toptal', email:'jhankar@gmail.com'}
-    
-
- ]
+]
 
 
 app.get('/', (req, res) => {
-  res.send('How To pooost request and bankEnd server')
+  res.send('How To pooost requestss and bankEnd server')
 })
 
 app.get('/user', (req, res) => {
@@ -28,6 +26,12 @@ app.get('/user', (req, res) => {
 app.post('/user', (req, res) => {
 
    console.log(req.body)
+   const newUser = req.body
+   newUser.id = user.length + 1;
+   user.push(newUser)
+   res.send(newUser)
+
+
 
 })
 
