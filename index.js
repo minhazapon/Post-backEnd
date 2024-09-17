c87f1b5c8956a4afbcf254fa2119cf07f86a25fc
minhazapon <minhajulabedinapon@gmail.com>
2024-09-16 22:22:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.port || 5000
+
+app.use(cors())
+
+  
+ const user = [
+     
+     { id:1, name:'minhazul abedin', position:'full stack Developer', language:'javasCript', company:'toptal', email:'apon@gmail.com'},
+     { id:2, name:'Hm nayeem', position:'full stack Developer', language:'javasCript', company:'toptal', email:'nayeem@gmail.com'},
+     { id:3, name:'Jhankar mahbub', position:'full stack Developer', language:'javasCript', company:'toptal', email:'jhankar@gmail.com'}
+    
+
+ ]
+
+
+app.get('/', (req, res) => {
+  res.send('How To pOst request and bankEnd server')
+})
+
+app.get('/user', (req, res) => {
+  res.send(user)
+})
+
+
+app.listen(port, () => {
+  console.log(`post Port ---> ${port}`)
+})
